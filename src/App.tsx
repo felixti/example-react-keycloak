61b522128f86c032339315bfb26ca1b3637f78f4
Felipe Felix <dev@MacBook-Pro-de-Felipe.local>
2019-08-05 11:53:48 -0300

    feat: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useKeycloak } from 'react-keycloak';
+import axios from 'axios'
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { keycloak, initialized } = useKeycloak()
+  console.log(keycloak.token)
+  console.log(initialized)
+  const [values, setValues] = useState([])
+ console.log(values)
+  useEffect(() => { 
+    axios.get('https://localhost:5001/api/values', { headers: { Authorization: `bearer ${keycloak.token}`}})
+    .then(response => response.data)
+    .then(data => setValues(data))
+  }, [keycloak])
+
+  // const claims = keycloak.token && jwtDecode<any>(keycloak.token as string)
+  return keycloak.authenticated ? (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {`${values[0]}-${values[1]}`}
+          </a>
+        </header>
+  </div> )
+    :
+    (
+      <div>Loading...</div>
+    )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Keycloak from 'keycloak-js'
+import { KeycloakProvider } from 'react-keycloak'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const keycloak = Keycloak({
+  "realm": process.env.REACT_APP_REALM,
+  "url": process.env.REACT_APP_IDENTITY_SERVER_URL,
+  "auth-server-url":process.env.REACT_APP_IDENTITY_SERVER_URL,
+  "clientId": process.env.REACT_APP_CLIENT_ID,
+  "resource": process.env.REACT_APP_CLIENT_ID
+})
+
+const onKeycloakEvent = (event: any, error: any) => {
+  console.log('onKeycloakEvent', event, error);
+  event === 'onAuthLogout' && localStorage.removeItem('kcTokens');
+  event === 'onTokenExpired' && updateToken();
+}
+
+const onKeycloakTokens = (tokens:any) => {
+  console.log({ tokens });
+  localStorage.setItem('kcTokens', JSON.stringify(tokens));
+}
+
+ReactDOM.render(
+      <KeycloakProvider 
+        keycloak={keycloak}
+        initConfig={{
+          onLoad: 'login-required',
+          flow: 'implicit'
+        }}
+        onEvent={onKeycloakEvent}
+        onTokens={onKeycloakTokens}>
+        <App />
+      </KeycloakProvider>, document.getElementById('root'));
+
+const updateToken = (() => {
+  keycloak.updateToken(180).success((refreshed) => {
+    if (refreshed) {
+        console.debug('Token refreshed' + refreshed);
+    } else {
+        console.warn('Token not refreshed, valid for')
+    }
+  }).error((err) => {
+      console.error('Failed to refresh token: ' + err);
+  });
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
